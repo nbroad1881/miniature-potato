a639dd715e5f79a1f309434edb2cf74096cea932
Nicholas Broad <nicholas@nmbroad.com>
2022-07-09 10:56:48 -0400

    refactor, add predictfirstmodule

--- data.py
+++ data.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from typing import Any, Optional, Tuple
 
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import (
     StratifiedGroupKFold,
     GroupKFold,
@@ -29,64 +30,33 @@ class DataModule:
         if self.cfg is None:
             raise ValueError("Please provide a config file")
 
-        self.data_dir = Path(self.cfg["data_dir"])
+        (
+            self.data_dir,
+            self.tokenizer,
+            self.orders_ds,
+            self.fold_idxs,
+            self.ds,
+        ) = setup_data(self.cfg)
 
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.cfg["model_name_or_path"],
-            use_auth_token=os.environ.get("HUGGINGFACE_HUB_TOKEN", True),
-        )
-
-        self.cls_tkn_map = {"code": "[CLS_CODE]", "markdown": "[CLS_MARKDOWN]"}
+        cls_tkn_map = {"code": "[CLS_CODE]", "markdown": "[CLS_MARKDOWN]"}
 
         self.tokenizer.add_special_tokens(
-            {"additional_special_tokens": list(self.cls_tkn_map.values())}
+            {"additional_special_tokens": list(cls_tkn_map.values())}
         )
         self.cls_id_map = {
-            "code": self.tokenizer.encode(self.cls_tkn_map["code"])[1],
-            "markdown": self.tokenizer.encode(self.cls_tkn_map["markdown"])[1],
+            "code": self.tokenizer.encode(cls_tkn_map["code"])[1],
+            "markdown": self.tokenizer.encode(cls_tkn_map["markdown"])[1],
         }
-
-        if self.cfg["load_from_disk"]:
-            if self.cfg["load_from_disk"].endswith(".dataset"):
-                load_path = self.cfg["load_from_disk"][: -len(".dataset")]
-            else:
-                load_path = self.cfg["load_from_disk"]
-            self.ds = load_from_disk(f"{load_path}.dataset")
-
-            with open(f"{load_path}.pkl", "rb") as fp:
-                self.fold_idxs = pickle.load(fp)
-            print("Loading dataset from disk", self.cfg["load_from_disk"])
-            if self.cfg["DEBUG"]:
-                self.ds = self.ds.select(range(self.cfg["n_debug"]))
-        else:
-            self.orders_ds = load_dataset(
-                "csv", data_files=str(self.data_dir / "train_orders.csv"), split="train"
-            )
-
-            if self.cfg["DEBUG"]:
-                self.orders_ds = self.orders_ds.select(range(self.cfg["n_debug"]))
-
-            self.orders_ds = self.orders_ds.map(
-                lambda x: {"length": [len(x.split()) for x in x["cell_order"]]},
-                batched=True,
-                num_proc=self.cfg["num_proc"],
-                desc="Calculating length of cell orders",
-            )
-
-            self.fold_idxs = get_folds(
-                self.orders_ds.to_pandas(),
-                pd.read_csv(self.data_dir / "train_ancestors.csv"),
-                k_folds=self.cfg["k_folds"],
-                stratify_on=self.cfg["stratify_on"],
-                groups=self.cfg["fold_groups"],
-            )
 
     def prepare_datasets(self):
 
         if self.cfg["load_from_disk"] is None:
 
             self.ds = self.orders_ds.map(
-                self.read_text_files, batched=False, num_proc=self.cfg["num_proc"]
+                read_text_files,
+                batched=False,
+                num_proc=self.cfg["num_proc"],
+                fn_kwargs={"data_dir": self.data_dir},
             )
 
             print(sum(self.ds["error"]), "errors")
@@ -105,7 +75,7 @@ class DataModule:
                 batched=False,
                 num_proc=self.cfg["num_proc"],
                 desc="Adding CLS tokens",
-                fn_kwargs={"max_length":self.cfg["max_length"]}
+                fn_kwargs={"max_length": self.cfg["max_length"]},
             )
 
             self.ds.save_to_disk(f"{self.cfg['output']}.dataset")
@@ -122,36 +92,6 @@ class DataModule:
         idxs = self.fold_idxs[fold]
         # print("Unique eval fold values:", self.raw_ds.select(idxs).unique("fold"))
         return self.ds.select(idxs)
-
-    def read_text_files(self, example):
-
-        id_ = example["id"]
-
-        example["error"] = False
-
-        with open(self.data_dir / "train" / f"{id_}.json", "r") as fp:
-            data = json.load(fp)
-            try:
-                example["source"] = data["source"].values()
-                example["cell_type"] = data["cell_type"].values()
-
-                example["cell_ids"] = list(data["source"].keys())
-                example["correct_order"] = example["cell_order"].split()
-                cell_id2idx = {
-                    cell_id: example["correct_order"].index(cell_id)
-                    for cell_id in example["cell_ids"]
-                }
-
-                num_cells = len(example["cell_type"])
-                example["labels"] = [
-                    cell_id2idx[cell_id] / num_cells
-                    for cell_id in data["source"].keys()
-                ]
-
-            except:
-                example["error"] = True
-
-        return example
 
     def tokenize(self, example):
 
@@ -210,6 +150,165 @@ class DataModule:
         }
 
 
+@dataclass
+class PredictFirstDataModule:
+
+    cfg: dict = None
+
+    def __post_init__(self):
+        if self.cfg is None:
+            raise ValueError("Please provide a config file")
+
+        (
+            self.data_dir,
+            self.tokenizer,
+            self.orders_ds,
+            self.fold_idxs,
+            self.ds,
+        ) = setup_data(self.cfg)
+
+    def prepare_datasets(self):
+
+        if self.cfg["load_from_disk"] is None:
+
+            self.ds = self.orders_ds.map(
+                read_text_files,
+                batched=False,
+                num_proc=self.cfg["num_proc"],
+                fn_kwargs={"data_dir": self.data_dir},
+            )
+
+            print(sum(self.ds["error"]), "errors")
+
+            # disable_progress_bar()
+
+            self.ds = self.ds.map(
+                self.tokenize,
+                batched=False,
+                num_proc=self.cfg["num_proc"],
+                desc="Tokenizing",
+            )
+
+            self.ds.save_to_disk(f"{self.cfg['output']}.dataset")
+            with open(f"{self.cfg['output']}.pkl", "wb") as fp:
+                pickle.dump(self.fold_idxs, fp)
+
+            print("Saving dataset to disk:", self.cfg["output"])
+
+    def get_train_dataset(self, fold):
+        idxs = list(chain(*[i for f, i in enumerate(self.fold_idxs) if f != fold]))
+        return self.ds.select(idxs)
+
+    def get_eval_dataset(self, fold):
+        idxs = self.fold_idxs[fold]
+        # print("Unique eval fold values:", self.raw_ds.select(idxs).unique("fold"))
+        return self.ds.select(idxs)
+
+    def tokenize(self, examples):
+
+        zipped = zip(examples["source"], examples["cell_type"], examples["labels"])
+
+        all_labels = []
+        texts = []
+        for source, cell_type, labels in zipped:
+            true_first_idx = labels.index(0.0)
+            md_idxs = np.argwhere(np.array(cell_type, dtype=object)=="markdown")
+            idxs = list(set([true_first_idx]+np.random.choice(md_idxs, 3)))
+            
+            all_labels.extend([1 if i==true_first_idx else 0 for i in idxs ])
+            texts.extend([source[i] for i in idxs])
+
+        tokenized = self.tokenizer(
+            texts,
+            padding=False,
+            truncation=True,
+            max_length=self.cfg["max_length"]
+        )
+
+        tokenized["labels"] = all_labels
+
+        return tokenized
+
+
+def setup_data(cfg):
+    data_dir = Path(cfg["data_dir"])
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        cfg["model_name_or_path"],
+        use_auth_token=os.environ.get("HUGGINGFACE_HUB_TOKEN", True),
+    )
+
+    if cfg["load_from_disk"]:
+        if cfg["load_from_disk"].endswith(".dataset"):
+            load_path = cfg["load_from_disk"][: -len(".dataset")]
+        else:
+            load_path = cfg["load_from_disk"]
+        ds = load_from_disk(f"{load_path}.dataset")
+
+        with open(f"{load_path}.pkl", "rb") as fp:
+            fold_idxs = pickle.load(fp)
+        print("Loading dataset from disk", cfg["load_from_disk"])
+        if cfg["DEBUG"]:
+            ds = ds.select(range(cfg["n_debug"]))
+
+        orders_ds = None
+    else:
+        orders_ds = load_dataset(
+            "csv", data_files=str(data_dir / "train_orders.csv"), split="train"
+        )
+
+        if cfg["DEBUG"]:
+            orders_ds = orders_ds.select(range(cfg["n_debug"]))
+
+        orders_ds = orders_ds.map(
+            lambda x: {"length": [len(x.split()) for x in x["cell_order"]]},
+            batched=True,
+            num_proc=cfg["num_proc"],
+            desc="Calculating length of cell orders",
+        )
+
+        fold_idxs = get_folds(
+            orders_ds.to_pandas(),
+            pd.read_csv(data_dir / "train_ancestors.csv"),
+            k_folds=cfg["k_folds"],
+            stratify_on=cfg["stratify_on"],
+            groups=cfg["fold_groups"],
+        )
+        ds = None
+
+    return data_dir, tokenizer, orders_ds, fold_idxs, ds
+
+
+def read_text_files(example, data_dir):
+
+    id_ = example["id"]
+
+    example["error"] = False
+
+    with open(data_dir / "train" / f"{id_}.json", "r") as fp:
+        data = json.load(fp)
+        try:
+            example["source"] = data["source"].values()
+            example["cell_type"] = data["cell_type"].values()
+
+            example["cell_ids"] = list(data["source"].keys())
+            example["correct_order"] = example["cell_order"].split()
+            cell_id2idx = {
+                cell_id: example["correct_order"].index(cell_id)
+                for cell_id in example["cell_ids"]
+            }
+
+            num_cells = len(example["cell_type"])
+            example["labels"] = [
+                cell_id2idx[cell_id] / num_cells for cell_id in data["source"].keys()
+            ]
+
+        except:
+            example["error"] = True
+
+    return example
+
+
 def get_folds(
     orders_df, ancestors_df, k_folds=5, stratify_on="length", groups="ancestor_id"
 ):
@@ -252,7 +351,7 @@ class OnlyMaskingCollator(DataCollatorForLanguageModeling):
     BatchEncoding, with the `"special_tokens_mask"` key, as returned by a [`PreTrainedTokenizer`] or a
     [`PreTrainedTokenizerFast`] with the argument `return_special_tokens_mask=True`.
     </Tip>
-    
+
     See original source code here: https://github.com/huggingface/transformers/blob/8b332a6a160c6df82e4267aaf118d87377d78a67/src/transformers/data/data_collator.py#L607
     """
 
@@ -310,12 +409,12 @@ class OnlyMaskingCollator(DataCollatorForLanguageModeling):
 class AI4CodeDataCollator(DataCollatorForTokenClassification):
     """
     Data collator that will dynamically pad the inputs received, as well as the labels.
-    DataCollatorForTokenClassification does not have floats for the labels. 
+    DataCollatorForTokenClassification does not have floats for the labels.
     This must be used with the MaskingProbCallback that sets the environment
     variable at the beginning and end of the training step. This callback ensures
     that there is no masking done during evaluation.
 
-    see data collator source code here: https://github.com/huggingface/transformers/blob/8b332a6a160c6df82e4267aaf118d87377d78a67/src/transformers/data/data_collator.py#L264 
+    see data collator source code here: https://github.com/huggingface/transformers/blob/8b332a6a160c6df82e4267aaf118d87377d78a67/src/transformers/data/data_collator.py#L264
     """
 
     tokenizer = None
